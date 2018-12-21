ecb5a84449692c6d86f0be8acf1755030b99453b
Ike Njoku <ikeenjoku@gmail.com>
2018-12-21 23:56:32 +0100

    configure babel/eslint

--- src/app.js
+++ src/app.js
@@ -5,22 +5,22 @@ import logger from 'morgan';
 
 import modules from './modules';
 
-const app =  express();
+const app = express();
 
 app.use(cors());
 app.use(logger('dev'));
 // body parser for url params and json
 app.use(bodyParser.urlencoded({
   limit: '50mb',
-  extended: true
+  extended: true,
 }));
 app.use(bodyParser.json());
 // set base url for api
 modules(app);
 
-//catch all routes
-app.use('*', (req, res) => {
-  return res.status(404).json({
-    message: 'Not Found. Use /api/v1 to access the API'
-  });
-});
+// catch all routes
+app.use('*', (req, res) => res.status(404).json({
+  message: 'Not Found. Use /api/v1 to access the API',
+}));
+
+export default app;
